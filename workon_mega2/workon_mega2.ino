3b9219f6d20ce41725650dcdca728a9ab1744f54
Chanasorn Siriwong <69181563+chanasorn1234@users.noreply.github.com>
2024-03-04 23:41:23 +0700

    Add files via upload

--- workon_mega2/workon_mega2.ino
+++ workon_mega2/workon_mega2.ino
@@ -2,11 +2,26 @@
 #define carbon_pin  A1
 #define voc_pin  A2
 #define arraySize 5
+#define active_button 12
+#define sig_to_runUno 7
+#define resetA9g_pin 8
+#define led_status1 2
+#define led_status2 3
+
 unsigned int pm2_5 = 0;
 unsigned int pm10 = 0;
 
+unsigned int limitOf_sentDataTo_cloud = 0; //limit at 5
+
+unsigned long startTime_uno = 0;
+unsigned long timeoutDuration_uno = 15000; // 5000 milliseconds = 5 seconds
+
+unsigned long startTime_a9g = 0;
+unsigned long timeoutDuration_a9g = 5000; // 5000 milliseconds = 5 seconds
+
 float rx_msg[5];//idx 0:avg_co2 , 1:avg_voc , 2:pm2_5 , 3:pm10
 bool receiving_data = false;
+bool system_run = false;
 
 //int countTofullArray = 0;
 //String resultToUno[arraySize];
@@ -15,101 +30,195 @@ int co2Offset = 100; // Offset value (adjust as needed)
 int co2Span = 200;   // Span value (adjust as needed)
 
 int sizeOfarray;
+
+String tx_msg_a9g = "";
+String rx_msg_a9g = "";
+//bool socket_a9g = false;
+
+
 void setup() {
   pinMode(carbon_pin,INPUT);
   pinMode(voc_pin,INPUT);
+  pinMode(active_button,INPUT);
+  pinMode(sig_to_runUno,OUTPUT);
+  digitalWrite(sig_to_runUno,HIGH);
   Serial.begin(115200);
   Serial1.begin(115200);//uno rx,tx on pin 19,18
   Serial2.begin(9600);//pm sensor rx,tx on pin 17,16
-
+  Serial3.begin(115200);//a9g module
+  
   calibrateMG811();
-
+  Serial.println("Wait For Response a9g Module");
+  delay(10000);
+  Init_a9g();
+  
+  Serial1.flush();
+  pinMode(resetA9g_pin,OUTPUT);
+  digitalWrite(resetA9g_pin,HIGH);
+  
+  pinMode(led_status1,OUTPUT);
+  pinMode(led_status2,OUTPUT);
+  
+  digitalWrite(led_status1,LOW);
+  digitalWrite(led_status2,LOW);
 }
 
 void loop() {
-  float avg_co2 = 0;
-  float avg_voc = 0;
 
-  int index_pm = 0;
-  char value_pm;
-  char previousValue_pm;
+  int inp = digitalRead(active_button);
+  if(inp == HIGH){
+    Serial.println("system is running");
+    Serial.println(limitOf_sentDataTo_cloud);
+    system_run = true;
+//    limitOf_sentDataTo_cloud = 2;
+  }
+
+
+  if(limitOf_sentDataTo_cloud >= 4){//check limit of data
+    Serial.println("Restart a9g board for Stability");
+    digitalWrite(resetA9g_pin,LOW);//}
+    delay(500);                     //}  reset a9g module board by hardware
+    digitalWrite(resetA9g_pin,HIGH);//}
+//    socket_a9g = true;
+    delay(1000);
+    UpdateSerial_a9g();
+    delay(10000);
+    Init_a9g();
+    limitOf_sentDataTo_cloud = 0;
+  }
+
 
 
   
-  int co2Value = analogRead(carbon_pin);
-  avg_co2 = map(co2Value, 0, 1023, co2Offset, co2Span);
-
-
-  
-  for(int count_round2=0;count_round2<10;count_round2++){
-    float raw_data_voc = analogRead(voc_pin);
-    avg_voc += raw_data_voc;
-    delay(500);
-    if(count_round2 == 9){
-      avg_voc = avg_voc/10;
-
+  if(system_run == true){
+    digitalWrite(led_status1,HIGH);
+    digitalWrite(led_status2,LOW);
+    Serial.println(limitOf_sentDataTo_cloud);
+      float avg_co2 = 0;
+      float avg_voc = 0;
+    
+      int index_pm = 0;
+      char value_pm;
+      char previousValue_pm;
+    
+    
+      
+      int co2Value = analogRead(carbon_pin);
+      avg_co2 = map(co2Value, 0, 1023, co2Offset, co2Span);
+    
+    
+      
+      for(int count_round2=0;count_round2<10;count_round2++){
+        float raw_data_voc = analogRead(voc_pin);
+        avg_voc += raw_data_voc;
+        delay(500);
+        if(count_round2 == 9){
+          avg_voc = avg_voc/10;
+    
+          }
       }
-  }
-
-  while (Serial2.available() || index_pm==0) {
-  value_pm = Serial2.read();
-  if ((index_pm == 0 && value_pm != 0x42) || (index_pm == 1 && value_pm != 0x4d)){
-    Serial.println("Cannot find the data header.");
-    index_pm = 0;
-    //break;
-  }
-   
-  if (index_pm == 4 || index_pm == 6 || index_pm == 8 || index_pm == 10 || index_pm == 12 || index_pm == 14) {
-    previousValue_pm = value_pm;
-  }
-
-  else if (index_pm == 7) {
-    pm2_5 = 256 * previousValue_pm + value_pm;
-
-
-  }
-  else if (index_pm == 9) {
-    pm10 = 256 * previousValue_pm + value_pm;
-
-
-  } 
-  else if (index_pm > 15) {
-    break;
-  }
-  index_pm++;
-}
-  while(Serial2.available()) Serial2.read();
-//  Serial.println(" }");
-
-  Serial.print("Carbon: ");
-  Serial.println(avg_co2);
-  
-  Serial.print("Voc: ");
-  Serial.println(avg_voc);
-
-  Serial.print("pm2_5: ");
-  Serial.print(pm2_5);
-  Serial.println(" ug/m3");
-
-  Serial.print("pm10: ");
-  Serial.print(pm10);
-  Serial.println(" ug/m3");
-
-
-
-  
-
-  receiving_data = true;
-  while(receiving_data == true){
-    while(Serial1.available()>0){
-      String receivedData = Serial1.readStringUntil('\n');
-//      Serial.println(receivedData);
-      splitStringToArray(receivedData);
-
+    
+      while (Serial2.available() || index_pm==0) {
+      value_pm = Serial2.read();
+      if ((index_pm == 0 && value_pm != 0x42) || (index_pm == 1 && value_pm != 0x4d)){
+        Serial.println("Cannot find the data header.");
+        index_pm = 0;
+        //break;
+      }
+       
+      if (index_pm == 4 || index_pm == 6 || index_pm == 8 || index_pm == 10 || index_pm == 12 || index_pm == 14) {
+        previousValue_pm = value_pm;
+      }
+    
+      else if (index_pm == 7) {
+        pm2_5 = 256 * previousValue_pm + value_pm;
+    
+    
+      }
+      else if (index_pm == 9) {
+        pm10 = 256 * previousValue_pm + value_pm;
+    
+    
+      } 
+      else if (index_pm > 15) {
+        break;
+      }
+      index_pm++;
     }
-  }
-  displayArray();
+      while(Serial2.available()) Serial2.read();
+    //  Serial.println(" }");
+    
+      Serial.print("Carbon: ");
+      Serial.println(avg_co2);
+      
+      Serial.print("Voc: ");
+      Serial.println(avg_voc);
+    
+      Serial.print("pm2_5: ");
+      Serial.print(pm2_5);
+      Serial.println(" ug/m3");
+    
+      Serial.print("pm10: ");
+      Serial.print(pm10);
+      Serial.println(" ug/m3");
+
+
+      Serial1.flush();
+      digitalWrite(led_status1,LOW);
+      digitalWrite(led_status2,HIGH);
+      digitalWrite(sig_to_runUno,LOW);//cmd to run another Board
+      delay(1000);
+      digitalWrite(sig_to_runUno,HIGH);
+      Serial.println("Wait data from another board");
+      delay(10000);
+      
+      startTime_uno = millis();
+      while(millis() - startTime_uno < timeoutDuration_uno){
+        if(Serial1.available()>0){
+          String receivedData = Serial1.readStringUntil('\n');
+          splitStringToArray(receivedData);
+          break;
+        }
+      }
+      
+      displayArray();
+
+      if(sizeOfarray == arraySize){
+
+      digitalWrite(led_status1,HIGH);
+      digitalWrite(led_status2,HIGH);
+      
+      tx_msg_a9g = "AT+HTTPGET=\"https://api.thingspeak.com/update?api_key=3921T8B66R9QH7BR";
+      tx_msg_a9g += "&field1=";
+      tx_msg_a9g += String(rx_msg[0]);
+      tx_msg_a9g += "&field2=";
+      tx_msg_a9g += String(pm10);
+      tx_msg_a9g += "&field3=";
+      tx_msg_a9g += String(pm2_5);
+      tx_msg_a9g += "&field4=";
+      tx_msg_a9g += String(avg_co2);
+      tx_msg_a9g += "&field5=";
+      tx_msg_a9g += String(avg_voc);
+      tx_msg_a9g += "\"\r";
+      
+      Serial3.println(tx_msg_a9g);
+      Serial.println(tx_msg_a9g);
+      delay(10000);
+//      socket_a9g = true;
+      UpdateSerial_a9g();
+//      system_run = false;
+      limitOf_sentDataTo_cloud += 1;
+      }
+      else{Serial.println(sizeOfarray);}
+      Serial.println("Please wait for 60 sec");
+      digitalWrite(led_status1,LOW);
+      digitalWrite(led_status2,LOW);
+      delay(60000);
+      
+
   
+  }
+//  system_run = false;
 }
 
 
@@ -139,15 +248,10 @@ void splitStringToArray(String input) {
   }
   // Add the last element to the array
   rx_msg[index] = input.toFloat();
-  receiving_data = false;
+//  receiving_data = false;
   sizeOfarray = index+1;
 
 }
-
-
-
-
-
 
 
 void calibrateMG811() {
@@ -164,4 +268,56 @@ void calibrateMG811() {
 
   co2Offset = sum / numSamples;
   co2Span = co2Offset + 400;  // Adjust span value as needed
+}
+
+void Init_a9g(){
+  
+  Serial3.println("AT\r");
+  delay(1000);
+//  socket_a9g = true;
+  UpdateSerial_a9g();
+
+  Serial3.println("AT+CGATT=1\r");
+  delay(1000);
+//  socket_a9g = true;
+  UpdateSerial_a9g();
+
+  Serial3.println("AT+CGDCONT=1,\"IP\",\"TRUE-H INTERNET\"");
+  delay(1000);
+//  socket_a9g = true;
+  UpdateSerial_a9g();
+
+  Serial3.println("AT+CGACT=1,1");
+  delay(1000);
+//  socket_a9g = true;
+  UpdateSerial_a9g();
+
+  Serial3.println("AT+CIFSR");
+  delay(1000);
+//  socket_a9g = true;
+  UpdateSerial_a9g();
+}
+
+void UpdateSerial_a9g()
+{
+//  startTime_a9g = millis();
+//  while(millis() - startTime_a9g < timeoutDuration_a9g){
+  //while(socket_a9g == true){
+   while (Serial3.available()>0){  
+    char val = Serial3.read();
+//    rx_msg_a9g += val;
+    if(val == '\n'){
+      Serial.println(rx_msg_a9g);
+//      Serial.flush();
+      rx_msg_a9g = "";
+//      socket_a9g = false;
+//      break;
+    }
+    else{
+      rx_msg_a9g += val;
+      
+    }
+    }
+//   while (Serial.available())   Serial3.write(Serial.read());
+  //}
 }
--- workon_uno2/workon_uno2.ino
+++ workon_uno2/workon_uno2.ino
@@ -11,11 +11,16 @@ DFRobot_GAS_I2C gas(&Wire ,I2C_ADDRESS);
 #define ch4_pin A0 
 #define arraySize 5
 #define mq_135_pin A1
+#define sig_to_run 7
 //float tx_msg[5];//idx 0:avg_co2 , 1:avg_voc , 2:pm2_5 , 3:pm10
 bool receiving_data = false;
+bool system_run = false;
 int sizeOfarray;
 unsigned int pm2_5 = 0;
 unsigned int pm10 = 0;
+
+unsigned long startTime_gps = 0;
+unsigned long timeoutDuration_gps = 1000; // 5000 milliseconds = 5 seconds
 
 float latitude;
 float lntitude;
@@ -28,9 +33,11 @@ SoftwareSerial ss(4, 5);
 TinyGPSPlus gps;
 void setup() {
   pinMode(ch4_pin,INPUT);
+  pinMode(sig_to_run,INPUT_PULLUP);
+  pinMode(mq_135_pin,INPUT);
   Serial.begin(115200);
   mega.begin(115200);
-  ss.begin(115200);
+  ss.begin(9600);
  while(!gas.begin())
   {
     Serial.println("NO Deivces CO!");
@@ -40,156 +47,110 @@ void setup() {
   gas.changeAcquireMode(gas.PASSIVITY);
   gas.setTempCompensation(gas.ON);
 Serial.println("The device is connected successfully!");
+mega.flush();
 }
 
 void loop() {
-  float avg_ch4 = 0;
-  float avg_mq_135 = 0;
-  countTofullArray = 0;
 
-  //Methene
-  for(int count_round=0;count_round<10;count_round++){ 
-    float raw_data_ch4 = analogRead(ch4_pin);
-    avg_ch4 += raw_data_ch4;
-    delay(500);
-    if(count_round == 9){
-      avg_ch4 = avg_ch4/10;
-      Serial.print("CH4: ");
-      Serial.print(avg_ch4);
-      Serial.println("PPM");
-      resultToMega[countTofullArray]=avg_ch4;
-      countTofullArray++;
-      }
-  }
-
-  //Mq-135
-  for(int count_round2=0;count_round2<10;count_round2++){ 
-    float raw_data_mq_135 = analogRead(mq_135_pin);
-    avg_mq_135 += raw_data_mq_135;
-    delay(500);
-    if(count_round2 == 9){
-      avg_mq_135 = avg_mq_135/10;
-      Serial.print("mq_135: ");
-      Serial.print(avg_mq_135);
-      Serial.println("PPM");
-      resultToMega[countTofullArray]=avg_mq_135;
-      countTofullArray++;
-      }
-  }
-
-  //gps
- while (ss.available() > 0){
-      gps.encode(ss.read());
-      if (gps.location.isUpdated()){
-      latitude = gps.location.lat();
-      lntitude = gps.location.lng();
-      Serial.print("Latitude= ");
-      Serial.print(latitude, 6);
-      Serial.print(" Longitude= ");
-      Serial.println(lntitude, 6);
-      }
-  }
-
-      latitude = 2;
-      lntitude = 3;
-      Serial.print("Latitude= ");
-      Serial.print(latitude);
-      Serial.print(" Longitude= ");
-      Serial.println(lntitude);
-      resultToMega[countTofullArray]=latitude;
-      countTofullArray++;
-      resultToMega[countTofullArray]=lntitude;
-      countTofullArray++;
+  int input_from_D13 = digitalRead(sig_to_run);
+//  Serial.println(input_from_D13);
+  if(input_from_D13 == 0){
+    system_run = true;
+    Serial.println("System is running");
+    }
 
   
-  Serial.print("Ambient ");
-  Serial.print(gas.queryGasType());
-  Serial.print(" concentration is: ");
-  Serial.print(gas.readGasConcentrationPPM());
-  Serial.println(" PPM");
-  resultToMega[countTofullArray]=gas.readGasConcentrationPPM();
-  countTofullArray++;
 
-
-
-
-
-      Serial.println(countTofullArray);//set format to send data
-      String combinedResult = "";
-      if(countTofullArray == arraySize){
-        for (int i = 0; i < arraySize; i++) {
-          combinedResult += resultToMega[i];
-          if (i < arraySize - 1) {
-            combinedResult += ","; // Add a space between strings
+  if(system_run == true){
+      float avg_ch4 = 0;
+      float avg_mq_135 = 0;
+      countTofullArray = 0;
+    
+      //Methene
+      for(int count_round=0;count_round<10;count_round++){ 
+        float raw_data_ch4 = analogRead(ch4_pin);
+        avg_ch4 += raw_data_ch4;
+        delay(500);
+        if(count_round == 9){
+          avg_ch4 = avg_ch4/10;
+          Serial.print("CH4: ");
+          Serial.print(avg_ch4);
+          Serial.println("PPM");
+          resultToMega[countTofullArray]=avg_ch4;
+          countTofullArray++;
           }
-        }
-      
-        // Send the combined string through SoftwareSerial
-        Serial.println(combinedResult);
-        mega.print(combinedResult);
       }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  ///add code comand to mega for sampling /// 
-//  receiving_data = true;
-//  
-//  while(receiving_data == true){
-//    while(mega.available()>0){
-//      String receivedData = mega.readStringUntil('\n');
-////      Serial.println(receivedData);
-//      splitStringToArray(receivedData);
-//
-//    }
-//  }
-//  displayArray();
-  
+    
+      //Mq-135
+      for(int count_round2=0;count_round2<10;count_round2++){ 
+        float raw_data_mq_135 = analogRead(mq_135_pin);
+        avg_mq_135 += raw_data_mq_135;
+        delay(500);
+        if(count_round2 == 9){
+          avg_mq_135 = avg_mq_135/10;
+          Serial.print("mq_135: ");
+          Serial.print(avg_mq_135);
+          Serial.println("PPM");
+          resultToMega[countTofullArray]=avg_mq_135;
+          countTofullArray++;
+          }
+      }
+    
+      //gps
+      startTime_gps = millis();
+     while(millis() - startTime_gps < timeoutDuration_gps){
+//      Serial.println("wait for gps");
+          if(ss.available()>0){
+          gps.encode(ss.read());
+          if (gps.location.isUpdated()){
+            latitude = gps.location.lat();
+            lntitude = gps.location.lng();
+//            Serial.print("Latitude= ");
+//            Serial.print(latitude, 6);
+//            Serial.print(" Longitude= ");
+//            Serial.println(lntitude, 6);
+            break;
+          }
+      }
+     }
+//          latitude = 2;
+//          lntitude = 3;
+          Serial.print("Latitude= ");
+          Serial.print(latitude,6);
+          Serial.print(" Longitude= ");
+          Serial.println(lntitude,6);
+          resultToMega[countTofullArray]=latitude;
+          countTofullArray++;
+          resultToMega[countTofullArray]=lntitude;
+          countTofullArray++;
+    
+      
+      Serial.print("Ambient ");
+      Serial.print(gas.queryGasType());
+      Serial.print(" concentration is: ");
+      Serial.print(gas.readGasConcentrationPPM());
+      Serial.println(" PPM");
+      resultToMega[countTofullArray]=gas.readGasConcentrationPPM();
+      countTofullArray++;
+    
+    
+          Serial.println(countTofullArray);//set format to send data
+          String combinedResult = "";
+          if(countTofullArray == arraySize){
+            for (int i = 0; i < arraySize; i++) {
+              combinedResult += resultToMega[i];
+              if (i < arraySize - 1) {
+                combinedResult += ","; // Add a space between strings
+              }
+            }
+          
+            // Send the combined string through SoftwareSerial
+            Serial.println(combinedResult);
+            mega.print(combinedResult);
+          }
+    
+    system_run = false;
+    Serial.println("System is done");
+    delay(10000);
+  }
 }
-
-
-//void splitStringToArray(String input) {
-//  int index = 0; // Index for the dataArray
-//  int spaceIndex = 0; // Variable to store the position of the space character
-////  Serial.println(input.substring(0, 6));
-// 
-//  while (spaceIndex != -1) {
-//    spaceIndex = input.indexOf(","); // Find the position of the space character
-//    
-//    if(spaceIndex != -1){
-//      rx_msg[index] = input.substring(0, spaceIndex).toFloat(); // Convert to float and add to array
-//      input = input.substring(spaceIndex + 1); // Update input string to exclude the processed part
-//      index++;
-//    }
-//  }
-//  // Add the last element to the array
-//  rx_msg[index] = input.toFloat();
-//  receiving_data = false;
-//  sizeOfarray = index+1;
-//
-//}
-//
-//void displayArray() {
-//  Serial.println("Contents of the array:");
-////  Serial.println(sizeof(rx_msg));
-//  for (int i = 0; i < sizeOfarray; i++) {
-//      Serial.println(rx_msg[i]);
-//  }
-//}
